0cbfc6cb98fe913187963e2a35a01bf6c20d803e
AbdElAziz <abdelaziz524aziz@gmail.com>
2026-07-10 00:18:20 +0300

    new: Implement Graceful Shutdown

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -33,9 +37,30 @@ func main() {
 		ReadTimeout: time.Duration(cfg.Server.ReadTimeout) * time.Second,
 	}
 
-	log.Printf("Starting proxy on port %d\n", cfg.Server.Port)
+	go func() {
+		log.Printf("Starting proxy on port %d\n", cfg.Server.Port)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed: %v", err)
+	shutdownSig := make(chan os.Signal, 1)
+
+	signal.Notify(shutdownSig, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-shutdownSig
+	log.Printf("Received signal %v: Initiating graceful shutdown...\n", sig)
+
+	// Give inflight proxy connections (e.g., 15 seconds) to finish responding
+	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
+	defer cancel()
+
+	// this stops accepting new traffic and cleanly drains active connections
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Proxy forced to shutdown with errors: %v\n", err)
+	} else {
+		log.Println("Proxy server shutdown cleanly.")
 	}
+
+	log.Println("Application fully stopped.")
 }
